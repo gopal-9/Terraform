f592d79175b740559c50cc3d9cedbf08be48385b
gopal <kokkugopal19mail@example.com>
2026-02-11 12:27:43 +0530

    ec2 code

---
+++ vpc/subnet.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.az
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = var.az
+}
---
+++ vpc/vpc.tf
@@ -0,0 +1,11 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "prod-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
